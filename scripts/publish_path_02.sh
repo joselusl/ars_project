dc2293dcd109734a550a2f2f6911a82824038bc0
Jose-Luis SANCHEZ-LOPEZ <joseluis.sanlop@gmail.com>
2020-09-30 18:46:01 +0200

    preparing for next practice

---
+++ scripts/publish_path_02.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+echo "Publishing trajectory":
+
+
+rostopic pub -1 /robot_trajectory_ref nav_msgs/Path "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: 'world'
+poses:
+- header:
+    seq: 0
+    stamp:
+      secs: 0
+      nsecs: 0
+    frame_id: 'world'
+  pose:
+    position:
+      x: 0.0
+      y: 0.0
+      z: 1.0
+    orientation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+      w: 1.0
+- header:
+    seq: 0
+    stamp:
+      secs: 0
+      nsecs: 0
+    frame_id: 'world'
+  pose:
+    position:
+      x: 6.0
+      y: 0.0
+      z: 1.0
+    orientation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+      w: 1.0
+- header:
+    seq: 0
+    stamp:
+      secs: 0
+      nsecs: 0
+    frame_id: 'world'
+  pose:
+    position:
+      x: 6.0
+      y: 6.0
+      z: 1.0
+    orientation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+      w: 1.0
+- header:
+    seq: 0
+    stamp:
+      secs: 0
+      nsecs: 0
+    frame_id: 'world'
+  pose:
+    position:
+      x: 0.0
+      y: 6.0
+      z: 1.0
+    orientation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+      w: 1.0
+- header:
+    seq: 0
+    stamp:
+      secs: 0
+      nsecs: 0
+    frame_id: 'world'
+  pose:
+    position:
+      x: 0.0
+      y: 0.0
+      z: 1.0
+    orientation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+      w: 1.0" 
+
